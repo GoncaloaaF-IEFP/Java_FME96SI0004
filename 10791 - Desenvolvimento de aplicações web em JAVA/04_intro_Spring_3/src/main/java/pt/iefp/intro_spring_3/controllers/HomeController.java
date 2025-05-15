c785a7dfa7cab36c171bd2295e0381acda9de122
Gonçalo <goncaloaaf@gmail.com>
2025-05-15 14:33:17 +0100

    demo atributos -> HTML

---
+++ 10791 - Desenvolvimento de aplicações web em JAVA/04_intro_Spring_3/src/main/java/pt/iefp/intro_spring_3/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package pt.iefp.intro_spring_3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("nome", "Gonçalo");
+        return "index";
+    }
+
+    @GetMapping("/pag2")
+    public String pag2() {
+        return "pag2";
+    }
+
+}
---
+++ 10791 - Desenvolvimento de aplicações web em JAVA/04_intro_Spring_3/src/test/java/pt/iefp/intro_spring_3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iefp.intro_spring_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
